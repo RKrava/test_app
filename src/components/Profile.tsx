bd4ef295536f7760160a149931a0359ebff031ea
RKrava <krab9887@gmail.com>
2024-05-22 19:47:41 +0300

    preview

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,18 @@
-import { TonConnectButton } from "@tonconnect/ui-react";
+import {TonConnectButton, useTonWallet} from "@tonconnect/ui-react";
 import { useTonConnect } from "../hooks/useTonConnect";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../redux/store";
 import {useTelegram} from "../hooks/useTelegram";
 import {useEffect, useState} from "react";
 import {UserApi} from "../api/user.api";
 import {BotApi} from "../api/chat.api";
+import {setUserData} from "../redux/user/userSlice";
 
 export function Profile() {
     const {tg, tgUser} = useTelegram();
+    const dispatch = useDispatch()
+    const wallet = useTonWallet();
     const userData = useSelector((state: RootState) => state.user.userData);
-    const [selectedChannelId, setSelectedChannelId] = useState('')
     const [users, setUsers] = useState([] as any)
     const [keys, setKeys] = useState([] as any)
     const [connectedChannels, setConnectedChannels] = useState([] as any)
@@ -30,10 +32,14 @@ export function Profile() {
     useEffect(() => {
         updateUsers()
         updateKeys()
-        UserApi.getConnectedChannels(tgUser?.id.toString() ?? '').then(r => {
-            setConnectedChannels(r.data)
-        })
+        // UserApi.getConnectedChannels(tgUser?.id.toString() ?? '').then(r => {
+        //     setConnectedChannels(r.data)
+        // })
 
+        UserApi.getConnectedChannels('323693764').then(r => {
+            setConnectedChannels(r)
+            console.log(r)
+        })
     }, [])
 
     const myChannels = [
@@ -63,10 +69,6 @@ export function Profile() {
         }
     ]
 
-    const selectChannel = (channel_id: string) => {
-        setSelectedChannelId(channel_id)
-    }
-
     return (
         <div className="Container">
             <div>Username: {userData.userName}</div>
@@ -76,8 +78,12 @@ export function Profile() {
                 <div><button onClick={() => tg.openTelegramLink('https://t.me/talkiefi1_bot?startchannel=true&admin=invite_users+restrict_members')}>Add bot</button></div>
                 <p>Private channel: </p>
                 <div>
-                    {connectedChannels.map((item: any) => {
-                        return <p>{item.chatData.title} {selectedChannelId === item.channel_id ? <button disabled>Selected</button> : <button onClick={() => {selectChannel(item.channel_id)}}>Select</button>}</p>
+                    {connectedChannels?.map((item: any) => {
+                        return <p>{item.chatData.title} {userData.privateChannelId === item.channel_id
+                            ? <button disabled>Selected</button>
+                            : <button onClick={() =>
+                            {UserApi.updateUser(tgUser?.id.toString() ?? '', tgUser?.username ?? '', wallet?.account?.address ?? '', item.channel_id)
+                                .then(r => {dispatch(setUserData({...userData, privateChannelId: item.channel_id}))})}}>Select</button>}</p>
                     })}
                 </div>
             </div>
